707d60ba07b9c30ec1d07cc9e43cbcc5d1229732
Ates Goral <ates.g@myplanet.com>
2016-03-01 09:27:26 -0500

    Handle case where FIRSTDAYOFWEEK is null. Fixes #14.

--- angular-date-picker.js
+++ angular-date-picker.js
@@ -51,7 +51,9 @@
                     days = [], // Slices of this are used for ngRepeat
                     months = [],
                     daysOfWeek = [],
-                    firstDayOfWeek = ($locale.DATETIME_FORMATS.FIRSTDAYOFWEEK || 6) + 1 % 7;
+                    firstDayOfWeek = $locale.DATETIME_FORMATS.FIRSTDAYOFWEEK !== null
+                        ? ($locale.DATETIME_FORMATS.FIRSTDAYOFWEEK + 1) % 7
+                        : 0;
 
                 for (var i = 1; i <= 31; i++) {
                     days.push(i);
